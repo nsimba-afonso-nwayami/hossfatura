b2b42b1f5f298c78b0eab803af35bfe176087dd9
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-11 16:39:16 +0100

    Página funcionalidades feita

---
+++ src/pages/site/Funcionalidades.jsx
@@ -0,0 +1,18 @@
+import Banner from "../../components/funcionalidades/Banner";
+import FuncionalidadesCategoria from "../../components/funcionalidades/FuncionalidadesCategoria";
+import Comparacao from "../../components/funcionalidades/Comparacao";
+import CtaFinal from "../../components/funcionalidades/CtaFinal";
+
+export default function Funcionalidades() {
+  return (
+    <>
+      <title>Funcionalidades | HossFatura</title>
+
+      <Banner />
+      <FuncionalidadesCategoria />
+      <Comparacao />
+      <CtaFinal />
+
+    </>
+  );
+}
