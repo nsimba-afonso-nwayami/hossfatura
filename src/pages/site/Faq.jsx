b829f0be9ef0b88d9f28f60d320fe0e06712f9d6
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-12 09:41:38 +0100

    Página de perguntas criada e feita

---
+++ src/pages/site/Faq.jsx
@@ -0,0 +1,15 @@
+import Banner from "../../components/faq/Banner";
+import PerguntasFrequentes from "../../components/faq/PerguntasFrequentes";
+
+
+export default function Faq() {
+  return (
+    <>
+      <title>Perguntas Frequentes | HossFatura</title>
+
+      <Banner />
+      <PerguntasFrequentes />
+
+    </>
+  );
+}
