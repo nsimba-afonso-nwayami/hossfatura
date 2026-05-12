5b21d6fd4f9795a5fa708f5b3525263493e49c78
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-12 10:24:30 +0100

    Rotas das páginas de privacidade e termos adicionadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -13,6 +13,8 @@ import Planos from "../pages/site/Planos";
 import Demonstracao from "../pages/site/Demonstracao";
 import Faq from "../pages/site/Faq";
 import Contatos from "../pages/site/Contatos";
+import Termos from "../pages/site/Termos";
+import Privacidade from "../pages/site/Privacidade";
 import NotFound from "../pages/site/NotFound";
 
 //Autenticação
@@ -28,6 +30,8 @@ export default function AppRoutes() {
         <Route path="/demonstracao" element={<Demonstracao />} />
         <Route path="/perguntas-frequentes" element={<Faq />} />
         <Route path="/contatos" element={<Contatos />} />
+        <Route path="/termos" element={<Termos />} />
+        <Route path="/privacidade" element={<Privacidade />} />
         <Route path="*" element={<NotFound />} />
       </Route>
 
