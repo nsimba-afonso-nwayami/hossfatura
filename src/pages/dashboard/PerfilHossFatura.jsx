556a10515ed7e06fecc9679adc0224664fcf5676
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-14 09:49:43 +0100

    Página de perfil criada

---
+++ src/pages/dashboard/PerfilHossFatura.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef } from "react";
+import HossFaturaLayout from "./components/HossFaturaLayout";
+
+export default function PerfilHossFatura() {
+  return (
+    <>
+      <title>Meu Perfil | HossFatura</title>
+
+      <HossFaturaLayout title="Meu Perfil">
+        <section className="space-x-6">
+
+        </section>
+      </HossFaturaLayout>
+    </>
+  );
+}
