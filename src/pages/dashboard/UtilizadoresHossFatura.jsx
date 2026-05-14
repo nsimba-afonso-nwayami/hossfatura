282376bbd5b4fa71bde4ed958ac803bbab315450
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-14 09:39:29 +0100

    Página de utilizadores criada

---
+++ src/pages/dashboard/UtilizadoresHossFatura.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef } from "react";
+import HossFaturaLayout from "./components/HossFaturaLayout";
+
+export default function UtilizadoresHossFatura() {
+  return (
+    <>
+      <title>Utilizadores | HossFatura</title>
+
+      <HossFaturaLayout title="Utilizadoes">
+        <section className="space-x-6">
+
+        </section>
+      </HossFaturaLayout>
+    </>
+  );
+}
