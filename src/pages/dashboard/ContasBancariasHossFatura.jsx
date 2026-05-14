893e938f2860ae923b89d6551bf70bea55b73f4e
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-14 09:38:32 +0100

    Página de contas bancárias criada

---
+++ src/pages/dashboard/ContasBancariasHossFatura.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef } from "react";
+import HossFaturaLayout from "./components/HossFaturaLayout";
+
+export default function ContasBancariasHossFatura() {
+  return (
+    <>
+      <title>Contas Bancárias | HossFatura</title>
+
+      <HossFaturaLayout title="Contas Bancárias">
+        <section className="space-x-6">
+
+        </section>
+      </HossFaturaLayout>
+    </>
+  );
+}
