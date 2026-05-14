a1d6aa30478e5a79b534ecd34e212b45ce825f9a
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-05-14 09:39:08 +0100

    Página de relatórios criada

---
+++ src/pages/dashboard/RelatoriosHossFatura.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef } from "react";
+import HossFaturaLayout from "./components/HossFaturaLayout";
+
+export default function RelatoriosHossFatura() {
+  return (
+    <>
+      <title>Relatórios | HossFatura</title>
+
+      <HossFaturaLayout title="Relatórios">
+        <section className="space-x-6">
+
+        </section>
+      </HossFaturaLayout>
+    </>
+  );
+}
